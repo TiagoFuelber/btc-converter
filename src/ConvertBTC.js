023b7f51cb5abd2ff4ec8c9c065b72ea187c6ad1
Tiago Fuelber <tfuelber@gmail.com>
2018-04-05 13:55:53 -0300

    fixes linter errors

--- src/ConvertBTC.js
+++ src/ConvertBTC.js
@@ -13,15 +13,15 @@ function convertBTC(currency = 'USD', amount = 1) {
   spinner.start();
 
   return request(url)
-    .then(body => {
+    .then((body) => {
       spinner.stop();
       return body;
     })
-    .then(body => {
+    .then((body) => {
       const apiResponse = JSON.parse(body);
       console.info(`${chalk.red(amount)} BTC to ${chalk.cyan(currency)} = ${chalk.yellow(apiResponse.price)}`);
     })
-    .catch(err => {
+    .catch((err) => {
       spinner.stop();
       console.info(chalk.red('Something went wrong with the API.Try in a few minutes'));
       return err;
